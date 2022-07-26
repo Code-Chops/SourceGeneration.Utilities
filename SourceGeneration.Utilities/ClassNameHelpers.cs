815ae5d2dd7c20de14a50d58167c875d5bd6b0bb
Max Bergman <maxbergman@outlook.com>
2022-07-26 18:06:56 +0200

    0.0.5: Added class name helpers.

---
+++ SourceGeneration.Utilities/ClassNameHelpers.cs
@@ -0,0 +1,21 @@
+﻿namespace CodeChops.ImplementationDiscovery.SourceGeneration.Helpers;
+
+/// <summary>
+/// Provides helpers for class (or interface) names.
+/// </summary>
+public static class ClassNameHelpers
+{
+	public static string GetClassNameWithoutGenerics(string className)
+	{
+		var genericParameterIndex = className.IndexOf('<');
+		var classNameWithoutGenerics = genericParameterIndex <= 0 ? className : className.Substring(0, genericParameterIndex);
+
+		return classNameWithoutGenerics;
+	}
+
+	public static bool HasGenericParameter(string className)
+	{
+		var hasGenericParameter = className.Contains('<');
+		return hasGenericParameter;
+	}
+}
