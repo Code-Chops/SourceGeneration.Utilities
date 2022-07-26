8222be632fca887a92a7131515e5b321b4fd78c0
Max Bergman <maxbergman@outlook.com>
2022-07-26 17:26:15 +0200

    0.0.3: Fixed AttributeDataExtensions.TryGetArguments.

--- SourceGeneration.Utilities/Extensions/AttributeDataExtensions.cs
+++ SourceGeneration.Utilities/Extensions/AttributeDataExtensions.cs
@@ -8,12 +8,12 @@ public static class AttributeDataExtensions
 	/// <summary>
 	/// Tries to get the arguments of the attribute.
 	/// </summary>
-	public static bool TryGetArguments(this AttributeData attributeData, out Dictionary<string, ITypeSymbol>? argumentInfoByNames)
+	public static bool TryGetArguments(this AttributeData attributeData, out Dictionary<string, TypedConstant>? argumentConstantByNames)
 	{
 		var constructorParameters = attributeData.AttributeConstructor?.Parameters;
 		if (constructorParameters is null) 
 		{
-			argumentInfoByNames = null;
+			argumentConstantByNames = null;
 			return false; 
 		}
 
@@ -21,11 +21,40 @@ public static class AttributeDataExtensions
 		var argumentNames = constructorParameters.Value.Select(parameterSymbol => parameterSymbol.Name).ToList();
 
 		// Combine the argument names by their constant (retrieved from the constructor arguments).
-		var argumentNameAndTypePairs = attributeData.ConstructorArguments.Select((info, index) => (Name: argumentNames[index], Type: info.Type!));
+		var argumentNameAndTypePairs = attributeData.ConstructorArguments.Select((info, index) => (Name: argumentNames[index], Type: info));
 		
 		// Create a dictionary with the argument as key and a 
-		argumentInfoByNames = argumentNameAndTypePairs.ToDictionary(argument => argument.Name, argument => argument.Type);
+		argumentConstantByNames = argumentNameAndTypePairs.ToDictionary(argument => argument.Name, argument => argument.Type);
 		
+		return true;
+	}
+	
+	/// <summary>
+	/// Tries to get the arguments of the attribute.
+	/// </summary>
+	public static bool TryGetArgumentsA(
+		this AttributeData attributeData,
+		out Dictionary<string, (string Type, object? Value)>? argumentByNames)
+	{
+		argumentByNames = new Dictionary<string, (string Type, object? Value)>(StringComparer.OrdinalIgnoreCase);
+		var constructorParameters = attributeData.AttributeConstructor?.Parameters;
+
+		if (constructorParameters is null) 
+		{
+			return false; 
+		}
+
+		// Start with an indexed list of names for mandatory arguments
+		var argumentNames = constructorParameters.Value.Select(x => x.Name).ToList();
+
+		var allArguments = attributeData.ConstructorArguments
+			.Select((info, index) => new KeyValuePair<string, TypedConstant>(argumentNames[index], info));
+
+		foreach (var argument in allArguments)
+		{
+			argumentByNames.Add(argument.Key, (argument.Value.Type!.GetFullTypeNameWithGenericParameters(), argument.Value.Value));
+		}
+
 		return true;
 	}
 }
