4eef6f474405aed153d8d775377c65aa9a3af420
Max Bergman <maxbergman@outlook.com>
2022-08-17 22:43:56 +0200

    0.2.0: Added GetFileName.

---
+++ SourceGeneration.Utilities/FileNameHelpers.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace CodeChops.SourceGeneration.Utilities;
+
+public static class FileNameHelpers
+{
+	/// <summary>
+	/// Removes the root namespace of the source assembly from the file name.
+	/// <para>
+	/// For this to work, add: &lt;CompilerVisibleProperty Include="RootNamespace"/&gt; to an ItemGroup in the source assembly.
+	/// </para>
+	/// </summary>
+	/// <param name="fileName">The file name without the extension.</param>
+	public static string GetFileName(string fileName, AnalyzerConfigOptionsProvider configOptionsProvider)
+	{
+		configOptionsProvider.GlobalOptions.TryGetValue("build_property.RootNamespace", out var rootNamespace);
+		
+		if (rootNamespace is not null && fileName.Substring(0, rootNamespace.Length) == rootNamespace)
+			fileName = fileName.Substring(rootNamespace.Length + 1);
+
+		return $"{fileName}.g.cs";
+	}
+}
