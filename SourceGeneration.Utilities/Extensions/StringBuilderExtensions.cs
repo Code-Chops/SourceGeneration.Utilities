a12b231ac4e85ac593db78c3fcd4ccd9948b6cf1
Max Bergman <maxbergman@outlook.com>
2022-11-09 12:40:20 +0100

    0.2.5: Added trimEnd option to custom AppendLine.

--- SourceGeneration.Utilities/Extensions/StringBuilderExtensions.cs
+++ SourceGeneration.Utilities/Extensions/StringBuilderExtensions.cs
@@ -23,12 +23,15 @@ public static class StringBuilderExtensions
 	/// <summary>
 	/// Appends the string builder with text and a newline if the result of the provided method is not null.
 	/// </summary>
-	public static StringBuilder AppendLine(this StringBuilder sb, Func<string?> textRetriever)
+	public static StringBuilder AppendLine(this StringBuilder sb, Func<string?> textRetriever, bool trimEnd = false)
 	{
 		var text = textRetriever();
 
-		if (text is not null) 
+		if (text is not null)
+		{
 			sb.AppendLine(text);
+			if (trimEnd) sb.TrimEnd();
+		}
 
 		return sb;
 	}
